ced551974c329c10b8e85be2f1325ba1d63a0eff
NameTagz <flokander@gmail.com>
2014-12-07 22:25:15 +0100

    Teeny tiny script update

--- Assets/Scripts/TestCube.cs
+++ Assets/Scripts/TestCube.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class TestCube : MonoBehaviour {
 
 	void Start () {
-		transform.position = Grid.Position (transform.position.x, transform.position.y);
+		transform.position = Grid.Position (transform.position);
 	}
 
 	void Update () {

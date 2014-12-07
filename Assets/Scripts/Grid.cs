6e1e9f7ff4f27b526f5a7c70e9d5ba7e96cf4f6d
NameTagz <flokander@gmail.com>
2014-12-07 20:45:22 +0100

    Script Folder + GridScript

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	public Vector2 GridPosition (float x, float y) {
+		Vector2 vect = new Vector2();
+		vect.x = Mathf.Round (x);
+		vect.y = Mathf.Round (y);
+		return vect;
+	}
+
+}

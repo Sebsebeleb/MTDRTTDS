141f50ca8ecf3195d7aa71a1c038e9a86b6149af
NameTagz <flokander@gmail.com>
2014-12-07 21:43:55 +0100

    GridScript nameChange

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public static class Grid {
 
-	public static Vector2 GridPosition (float x, float y) {
+	public static Vector2 Position (float x, float y) {
 		Vector2 vect = new Vector2();
 		vect.x = Mathf.Round (x);
 		vect.y = Mathf.Round (y);

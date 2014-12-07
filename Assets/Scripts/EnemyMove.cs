23b981f9ab03347ddae1a30cd92eee40867bc799
Sebastian Vråle <seb@terminator.net>
2014-12-07 22:25:04 +0100

    Astar tweaking

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class EnemyMove : MonoBehaviour
+{
+    public Vector3 TargetPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Seeker seeker = GetComponent<Seeker>();
+	    seeker.StartPath(transform.position, TargetPosition, OnPathComplete);
+	}
+
+    void OnPathComplete(Path p)
+    {
+        Debug.Log("Hello path");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

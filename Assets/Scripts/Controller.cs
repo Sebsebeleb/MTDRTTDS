5dc863b0d4030ca40008211b577e88e19044e914
NameTagz <flokander@gmail.com>
2014-12-08 19:05:18 +0100

    Controller Script - PreviewTower + PlaceTower1.2

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -6,12 +6,19 @@ public class Controller : MonoBehaviour {
 
 	public GameObject TowerObj;
 	public LayerMask TowerBlock;
+
+	private SpriteRenderer renderer = new SpriteRenderer();
+
+	private Vector2 mousePos = new Vector2();
 	
 	void Start () {
-	
+		renderer = GetComponent<SpriteRenderer>();
 	}
 
 	void Update () {
+
+		GetMousePos();
+		PreviewTower();
 
 		if (Input.GetMouseButton (0)) {
 			PlaceTower();
@@ -19,14 +26,39 @@ public class Controller : MonoBehaviour {
 	
 	}
 
-	void PlaceTower () {
-		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	/// <summary>
+	/// Gets the mouse position in grid.
+	/// </summary>
+	void GetMousePos () {
+		mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		mousePos = Grid.Position (mousePos);
-		Collider2D col = Physics2D.OverlapPoint (mousePos, TowerBlock);
-		if ( col != null ) return;
+	}
+
+	/// <summary>
+	/// Places the tower on mousePos.
+	/// </summary>
+	void PlaceTower () {
+		Collider2D coll = Physics2D.OverlapPoint (mousePos, TowerBlock);
+		if ( coll != null ) return;
 
 		GameObject ins = Instantiate (TowerObj as Object) as GameObject;
 		ins.transform.position = Grid.Position (mousePos);
+	}
+
+	/// <summary>
+	/// Previews the tower on mousePos.
+	/// Also checks if placement is possible (no colliding obstacle) and colors preview thereafter.
+	/// </summary>
+	void PreviewTower () {
+		renderer.sprite = TowerObj.GetComponent<SpriteRenderer>().sprite;
+		Collider2D coll = Physics2D.OverlapPoint (mousePos, TowerBlock);
+		if ( coll == null ) {
+			renderer.material.SetColor ("_Color", Color.green);
+		}
+		else {
+			renderer.material.SetColor ("_Color", Color.red);
+		}
+		transform.position = mousePos;
 	}
 
 }
--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public static class Grid {
 
+	/// <summary>
+	/// Round a Vector2.
+	/// </summary>
+	/// <param name="vec">Vector2.</param>
 	public static Vector2 Position (Vector2 vec) {
 		Vector2 vect = new Vector2();
 		vect.x = Mathf.Round (vec.x);

7c0547b8e1f6296528729ee96595743e8bd0d80b
NameTagz <flokander@gmail.com>
2014-12-08 02:08:41 +0100

    Controller Script - PlaceTower

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class Controller : MonoBehaviour {
+
+	public GameObject TowerObj;
+	public LayerMask TowerBlock;
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		if (Input.GetMouseButton (0)) {
+			PlaceTower();
+		}
+	
+	}
+
+	void PlaceTower () {
+		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePos = Grid.Position (mousePos);
+		Collider2D col = Physics2D.OverlapPoint (mousePos, TowerBlock);
+		if ( col != null ) return;
+
+		GameObject ins = Instantiate (TowerObj as Object) as GameObject;
+		ins.transform.position = Grid.Position (mousePos);
+	}
+
+}
+

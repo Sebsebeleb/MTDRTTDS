1a593c4b9190a3953c3571ebb87c355c27a48ade
NameTagz <flokander@gmail.com>
2014-12-08 22:25:46 +0100

    Controller update - Tower Selection, ++

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -11,12 +11,17 @@ public class Controller : MonoBehaviour {
 
 	public bool Placing = false;
 
+	public Sprite SelectionSprite;
+	public GameObject Selected;
+
 	private SpriteRenderer renderer = new SpriteRenderer();
 
 	private Vector2 mousePos = new Vector2();
+	private Collider2D coll;
 	
 	void Start () {
 		renderer = GetComponent<SpriteRenderer>();
+		SetTower(TowerObj); //REMOVE ME LATER
 	}
 
 	void Update () {
@@ -35,22 +40,29 @@ public class Controller : MonoBehaviour {
 				renderer.sprite = null;
 			}
 		}
+		else {
+			if (Input.GetMouseButton (0)) {
+				GetTower();
+			}
+		}
 	
 	}
 
 	/// <summary>
 	/// Gets the mouse position in grid.
+	/// Also Overlap-checks position and saves colliders to "coll".
 	/// </summary>
 	void GetMousePos () {
 		mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		mousePos = Grid.Position (mousePos);
+		coll = Physics2D.OverlapPoint (mousePos, TowerBlock);
 	}
 
 	/// <summary>
 	/// Places the tower on mousePos.
 	/// </summary>
 	void PlaceTower () {
-		Collider2D coll = Physics2D.OverlapPoint (mousePos, TowerBlock);
+
 		if ( coll != null ) return;
 
 		GameObject ins = Instantiate (TowerObj as Object) as GameObject;
@@ -61,11 +73,8 @@ public class Controller : MonoBehaviour {
 
 	/// <summary>
 	/// Previews the tower on mousePos.
-	/// Also checks if placement is possible (no colliding obstacle) and colors preview thereafter.
 	/// </summary>
 	void PreviewTower () {
-		renderer.sprite = TowerObj.GetComponent<SpriteRenderer>().sprite;
-		Collider2D coll = Physics2D.OverlapPoint (mousePos, TowerBlock);
 		if ( coll == null ) {
 			renderer.material.SetColor ("_Color", Color.green);
 		}
@@ -73,6 +82,33 @@ public class Controller : MonoBehaviour {
 			renderer.material.SetColor ("_Color", Color.red);
 		}
 		transform.position = mousePos;
+	}
+
+	/// <summary>
+	/// Set the current tower object and other related variables.
+	/// </summary>
+	/// <param name="obj">Tower.</param>
+	void SetTower (GameObject obj) {
+		TowerObj = obj;
+		renderer.sprite = TowerObj.GetComponent<SpriteRenderer>().sprite;
+		Placing = true;
+	}
+
+	/// <summary>
+	/// Saves ID of object in "coll".
+	/// Also sets "Selected" to null if no object is found.
+	/// </summary>
+	void GetTower () {
+		if (coll != null) {
+			Selected = coll.gameObject;
+			renderer.material.SetColor ("_Color", Color.white);
+			renderer.sprite = SelectionSprite;
+			transform.position = mousePos;
+		}
+		else {
+			Selected = null;
+			renderer.sprite = null;
+		}
 	}
 
 }

84b72cbedd9733153a7f49ed9e1060b75c5af30a
NameTagz <flokander@gmail.com>
2014-12-10 14:10:23 +0100

    Masse random

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	public bool PowerOn = false;
+
+	private float Health = 100;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	void Damage (float amount) {
+		Health -= amount;
+		if (Health <= 0) {
+			Destroy(gameObject);
+		}
+	}
+
+}

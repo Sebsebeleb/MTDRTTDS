944f33ce47b351ddfcb93378b3c3d1731f14f210
NameTagz <flokander@gmail.com>
2014-12-10 16:10:53 +0100

    CameraControl

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		KeyboardMove();
+	
+	}
+
+	void KeyboardMove () {
+		Vector3 vect = transform.position;
+		if (Input.GetKey (KeyCode.A)) {
+			vect.x -= 0.2f;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			vect.x += 0.2f;
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			vect.y += 0.2f;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			vect.y -= 0.2f;
+		}
+		transform.position = vect;
+	}
+
+}
